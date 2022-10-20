300d7f7705892af0270078763053d0b5fc43f6a9
jf18j492 <jonas.frey@csh.unibe.ch>
2022-10-20 16:58:44 +0200

    more testdata

--- src/api/o-message/routes/o-message.js
+++ src/api/o-message/routes/o-message.js
@@ -1,4 +1,4 @@
-//{"s_msg":"this file has been written by file:///home/jf18j492/code/strapi_auto_generate_models/denojs/models/f_create_strapi_model.module.js","s_ts":"Thu Oct 20 2022 14:32:55 GMT+0200 (Central European Summer Time)","n_ts_ms":1666269175169}
+//{"s_msg":"this file has been written by file:///home/jf18j492/code/strapi_auto_generate_models/denojs/models/f_create_strapi_model.module.js","s_ts":"Thu Oct 20 2022 16:26:06 GMT+0200 (Central European Summer Time)","n_ts_ms":1666275966509}
 'use strict';
 /**
  * o-message router
